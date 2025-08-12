13ffabb010f90ad359dfefabcd33796b79e8b0c7
Balpreet Singh <balpreets10@gmail.com>
2025-08-12 22:14:41 +0530

    -- udpated eslint packages according to installed packages

--- gamingdronzz-website/eslint.config.mjs
+++ gamingdronzz-website/eslint.config.mjs
@@ -2,30 +2,31 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
 
-export default tseslint.config([
-  { ignores: ['dist'] }, // Fixed: was globalIgnores
+export default [
+  { ignores: ['dist'] },
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-    ],
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
     plugins: {
+      '@typescript-eslint': typescriptEslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...typescriptEslint.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
   },
-])
+]
